312d145a16843a9167a69fefff7cca586f9c45a5
Daniel Nunes <danielnunespk@gmail.com>
2024-05-15 13:30:29 -0300

    Add initial project setup
    Initialized project using Spring Initializr with Thymeleaf, Spring Web, and DevTools dependencies.

---
+++ src/main/java/com/danielnunesro/trainingmvc/MvcTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.danielnunesro.trainingmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielnunesro/trainingmvc/MvcTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielnunesro.trainingmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
